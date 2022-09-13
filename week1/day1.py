c5e9eecc871c64eca7362e5324304fccbdba0cc7
bobjay <boboj2004@gmail.com>
2022-09-12 23:28:22 -0500

    Day 1 of 100 days of python challenge

---
+++ week1/day1.py
@@ -0,0 +1,57 @@
+#Exercise 1 - Printing
+print("#Exercise 1 - Printing\n")
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')\n")
+
+#Exercise 2 - Debugging Practice
+print("#Exercise 2 - Debugging Practice\n")
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.\n")
+
+#Exercise 3 - Input Function)
+print("#Exercise 3 - Input Function\n")
+name = input("what is your name\n")
+print(len(name))
+print("\n")
+
+#Exercise 4 - Variables
+print("#Exercise 4 - Variables\n")
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+temp = a
+a = b
+b = temp
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b + "\n")
+
+#Day 1 Project
+print("#Day 1 Project")
+
+#1. Create a greeting for your program.
+print("Hello " + input("What is your name ? \n"))
+
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up ? \n")
+
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of your pet ? \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city +' ' + pet)
+
+#5. Make sure the input cursor shows on a new line.
+
