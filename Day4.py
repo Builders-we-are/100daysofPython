c91b3464eb81f90399172d20b7a7febf658f57c7
Rachel Iyare <rachel.iyare@hotmail.com>
2022-10-12 05:18:36 -0500

    Exercise2 Banker Rouleete

---
+++ Day4.py
@@ -0,0 +1,17 @@
+# Exercise2 Banker Rouleete- Who will pay the bill?
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# Using the split function allows you to separate a string components based on some sort of divider
+# Split function on a string in the output, will divide the components into a list, separating all the words by a comma and a space
+
+# print(names)
+# print(names[0]) #this will print the first item in the list which is Angela
+# now how do we get a random number to replace 0, first import random module then
+# print(len(names)) just to check
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)   # this bcos the number start with 0 & end with total num - 1
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today.")
