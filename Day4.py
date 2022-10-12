555c7dd5b7ed01aea3c11c5742109d21df8a1c33
Rachel Iyare <rachel.iyare@hotmail.com>
2022-10-12 05:46:49 -0500

    Exercise2 Banker Rouleete

--- Day4.py
+++ Day4.py
@@ -9,9 +9,9 @@ names = names_string.split(", ")
 # print(names)
 # print(names[0]) #this will print the first item in the list which is Angela
 # now how do we get a random number to replace 0, first import random module then
-# print(len(names)) just to check
+# print(len(names)) just to check the total nunmber of items in the list
 num_items = len(names)
-
-random_choice = random.randint(0, num_items - 1)   # this bcos the number start with 0 & end with total num - 1
-person_who_will_pay = names[random_choice]
+# generate random numbers between 0 and the last index
+# random_choice = random.randint(0, num_items - 1)   # this bcos the number start with 0 & end with total num - 1
+person_who_will_pay = random.choice(names)
 print(person_who_will_pay + " is going to buy the meal today.")
