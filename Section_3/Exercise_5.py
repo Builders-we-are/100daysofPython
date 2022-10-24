b9675724f273d73d918234b6567be86cd9480493
hadekunle <shitoheeb@gmail.com>
2022-10-24 15:59:34 -0500

    africa ascii art

--- Section_3/Exercise_5.py
+++ Section_3/Exercise_5.py
@@ -30,7 +30,27 @@ else:
 
 
 
+#Amazingly smart solution using for loops
 
+
+combined_names = (name1 + name2).lower()
+
+true_score = 0
+love_score = 0
+
+for letter in set("true"):
+  true_score += combined_names.count(letter)
+for letter in set("love"):
+  love_score += combined_names.count(letter)
+
+total_score = int(str(true_score) + str(love_score))
+
+if total_score < 10 or total_score > 90:
+  print(f"Your score is {total_score}, you go together like coke and mentos.")
+elif total_score >= 40 and total_score <= 50:
+  print(f"Your score is {total_score}, you are alright together.")
+else:
+  print(f"Your score is {total_score}")
 
 
 
