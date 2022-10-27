3f5a0ce88b427bd461f2935aab45cbe49d6c99a4
hadekunle <shitoheeb@gmail.com>
2022-10-27 13:56:09 -0500

    auto push

---
+++ Section_13/Day13.py
@@ -0,0 +1,101 @@
+###########DEBUGGING#####################
+
+# Describe Problem
+def my_function():
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+my_function()
+
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
+
+# Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994:
+  print("You are a Gen Z.")
+
+# Fix the Errors
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"You can drive at age {age}.")
+
+#Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
+
+# #Use a Debugger
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
+
+mutate([1,2,3,5,8,13])
+
+
+
+
+######## ADVICE TO DEBUGGING ########
+
+
+# 1.Describe the problem
+# 2. Reproduce the bug
+# 3. Play cpu
+# 4. Fix the error
+# 5. Use the print function
+# 6. Use a debugger (pythontutor.com)
+# 7. Take a break 
+# 8. Ask a friend (dev. or just avg. person)
+# 9. Run Often (use loops to iterate code)
+# 10. Stack Overflow
+
+
+
+#####  DEBUGGING ODD/EVEN NUMBER
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+#####  DEBUGGING LEAP YEAR
+
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+
+
+#####  DEBUGGING FIZZ BUZZ 
+# Debugging with the Print Statement 
+
+for number in range(1, 16):
+  print(f"Currently on number {number}")
+  if number % 3 == 0 or number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
