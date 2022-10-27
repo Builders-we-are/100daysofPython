17da55108972200ba2720be1bde009218e68d5de
hadekunle <shitoheeb@gmail.com>
2022-10-27 08:31:15 -0500

    auto push

--- Section_8/Exercise_2.py
+++ Section_8/Exercise_2.py
@@ -1,4 +1,5 @@
-#Write your code below this line 👇
+
+
 import math
 
 n = 11
@@ -14,14 +15,56 @@ def prime_checker(number):
         print("It's a prime number.")
         
 
-    
-
-                
-
-
-#Write your code above this line 👆
-    
 #Do NOT change any of the code below👇
 # n = int(input("Check this number: "))
 prime_checker(number=n)
+
+
+# George version ================================================
+
+def prime_checker(number):
+    for x in range(2,(number+1)):
+        if x < number and number % x == 0: 
+            print ("not prime")
+            # print(x)
+            break
+        elif x == number:
+            print ("prime")
+            break
+
+number = int(input("Check this number: "))
+prime_checker(number)
+
+
+
+# IK version ================================================
+
+import math
+
+def prime_checker(number):
+    really_prime = True
+    for x in range(2,n/2):
+        if number % x == 0:
+            really_prime = False
+    if really_prime == False:
+        print(f"{number} is not a prime number.")
+    else:
+        print(f"{number} is a prime number.")
+        
+ 
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
