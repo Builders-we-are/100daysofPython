780dd121e1c8fc6fc8e8c966af42615fe72d1564
hadekunle <shitoheeb@gmail.com>
2022-11-24 03:50:03 -0600

    python

--- Section_012/number_guess_game.py
+++ Section_012/number_guess_game.py
@@ -1,5 +1,41 @@
 
+import os
+os.system("clear")
+
+import math
+import random
+
+print("\n \n \n \n \n \n \n")
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+value = random.randint(0,100)
+print(f"\nPssst, the correct answer is {value} \n")
+
+mode = input("Choose a difficulty, Type 'easy' or 'hard': ").lower()
+mode_dict = {
+    'easy' : 10,
+    'hard' : 5, 
+}
+
+attempts = mode_dict[mode]
+print(f"You have {attempts} remaining to guess the number.")
+
+while attempts != 0 :
+    guess = int(input("\nMake a guess: "))
+    if guess == value:
+        print(f"You got it! The answer was {value} \n")
+        break
+    elif guess > value:
+        print("Too high. \nGuess again.")
+        attempts -= 1
+        print(f"You have {attempts} attempts remaining to guess the number.")
+    elif guess < value:
+        print("Too low. \nGuess again.")
+        attempts -= 1
+        print(f"You have {attempts} attempts remaining to guess the number.")
 
 
-
-print("Welcome to the guessing game! \nI'm thinking of a number between 1 and 100\nPsst, the correct number is ...\n")
+if attempts == 0 :
+    print("You've run out of guesses, you lose. \n")
