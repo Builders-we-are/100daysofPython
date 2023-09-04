fd85623d864ab353e26c7a4c4fafba1d7f2e130d
hadekunle <shitoheeb@gmail.com>
2023-09-04 01:53:51 -0500

    python

--- Section_026/main.py
+++ Section_026/main.py
@@ -1,27 +1,40 @@
+import pandas
 student_dict = {
-    "student": ["Angela", "James", "Lily"], 
+    "student": ["Angela", "James", "Lily"],
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
+# Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
+    # print(value)
+    # Access key and value
     pass
 
-import pandas
 student_data_frame = pandas.DataFrame(student_dict)
+# print(student_data_frame)
 
-#Loop through rows of a data frame
+# Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
+    # print(value)
+    # print(row)
+    # print(row["score"])
+    # print(row.score)
+    # Access index and row
+    # Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+nato = pandas.read_csv("./nato.csv")
+df = pandas.DataFrame(nato)
+# print(df)
+nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+# print(nato_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+name = input("Enter a word: ").upper
+result = [nato_dict[i.upper()] for i in name]
+print(result)
